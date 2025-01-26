8d19fcdb8684991d5e57855d95e5bddf8f5ce28e
shivam pathak <shivam1482002pathak@gmail.com>
2025-01-26 23:40:58 +0530

    Url change

--- Form_Builder_Frontend/src/services/formAPI.js
+++ Form_Builder_Frontend/src/services/formAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:3000/api/v1/forms";
+const API_URL = "https://formbuilder-tspl.onrender.com/api/v1/forms";
 
 // Fetch all forms
 export const getForms = async () => {
